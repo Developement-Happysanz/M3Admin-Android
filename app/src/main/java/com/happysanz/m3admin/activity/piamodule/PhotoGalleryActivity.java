151faf582de5adac877337574d942bbb7fae4df7
Naren-HappySanz <devika.happysanz@gmail.com>
2019-02-04 18:29:29 +0530

    Screen Designs

---
+++ app/src/main/java/com/happysanz/m3admin/activity/piamodule/PhotoGalleryActivity.java
@@ -0,0 +1,16 @@
+package com.happysanz.m3admin.activity.piamodule;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.happysanz.m3admin.R;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+
+}
