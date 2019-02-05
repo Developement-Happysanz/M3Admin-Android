7c2cf78d22612aea4d41df752a50cff904786751
Naren-HappySanz <devika.happysanz@gmail.com>
2019-02-05 15:36:04 +0530

    Screen Designs

--- app/src/main/java/com/happysanz/m3admin/activity/piamodule/PiaDashboard.java
+++ app/src/main/java/com/happysanz/m3admin/activity/piamodule/PiaDashboard.java
@@ -1,6 +1,7 @@
 package com.happysanz.m3admin.activity.piamodule;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -10,15 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.happysanz.m3admin.R;
 
-public class PiaDashboard extends AppCompatActivity {
+public class PiaDashboard extends AppCompatActivity implements View.OnClickListener {
     Toolbar toolbar;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     Context context;
     RelativeLayout prospect, user, addPlan, task, tracking, controlPanel, expView;
+    TextView scheme, center, project, trade, batch, tradeAndBatch, time;
     LinearLayout subMenu;
     Boolean visib = false;
     @Override
@@ -32,19 +35,34 @@ public class PiaDashboard extends AppCompatActivity {
         context = getApplicationContext();
         subMenu = (LinearLayout) findViewById(R.id.sub_menu);
         expView = (RelativeLayout) findViewById(R.id.exp_view);
-        expView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                visib = !visib;
-                if (visib) {
-                    subMenu.setVisibility(View.VISIBLE);
-                } else {
-                    subMenu.setVisibility(View.GONE);
-                }
-            }
-        });
+        expView.setOnClickListener(this);
+        prospect = (RelativeLayout) findViewById(R.id.pros_layout);
+        prospect.setOnClickListener(this);
+        user = (RelativeLayout) findViewById(R.id.user_layout);
+        user.setOnClickListener(this);
+        addPlan = (RelativeLayout) findViewById(R.id.add_plan_layout);
+        addPlan.setOnClickListener(this);
+        task = (RelativeLayout) findViewById(R.id.task_layout);
+        task.setOnClickListener(this);
+        tracking = (RelativeLayout) findViewById(R.id.tracking_layout);
+        tracking.setOnClickListener(this);
+        controlPanel = (RelativeLayout) findViewById(R.id.control_panel_layout);
+        controlPanel.setOnClickListener(this);
 
-
+        scheme = (TextView) findViewById(R.id.txt_scheme);
+        scheme.setOnClickListener(this);
+        project = (TextView) findViewById(R.id.txt_project);
+        project.setOnClickListener(this);
+        center = (TextView) findViewById(R.id.txt_center);
+        center.setOnClickListener(this);
+        trade = (TextView) findViewById(R.id.txt_trade);
+        trade.setOnClickListener(this);
+        batch = (TextView) findViewById(R.id.txt_batch);
+        batch.setOnClickListener(this);
+        tradeAndBatch = (TextView) findViewById(R.id.txt_trade_batch);
+        tradeAndBatch.setOnClickListener(this);
+        time = (TextView) findViewById(R.id.txt_time);
+        time.setOnClickListener(this);
     }
 
     private void initializeNavigationDrawer() {
@@ -97,4 +115,67 @@ public class PiaDashboard extends AppCompatActivity {
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view == expView){
+            visib = !visib;
+            if (visib) {
+                subMenu.setVisibility(View.VISIBLE);
+            } else {
+                subMenu.setVisibility(View.GONE);
+            }
+        }
+        if (view == prospect){
+            Intent intent = new Intent(getApplicationContext(), ProspectsActivity.class);
+            startActivity(intent);
+        }
+        if (view == user){
+            Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+            startActivity(intent);
+        }
+        if (view == addPlan){
+            Intent intent = new Intent(getApplicationContext(), AddPlanActivity.class);
+            startActivity(intent);
+        }
+        if (view == task){
+            Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+            startActivity(intent);
+        }
+        if (view == tracking){
+            Intent intent = new Intent(getApplicationContext(), TrackingActivity.class);
+            startActivity(intent);
+        }
+        if (view == controlPanel){
+            Intent intent = new Intent(getApplicationContext(), ControlPanelActivity.class);
+            startActivity(intent);
+        }
+        if (view == scheme){
+            Intent intent = new Intent(getApplicationContext(), SchemeActivity.class);
+            startActivity(intent);
+        }
+        if (view == center){
+            Intent intent = new Intent(getApplicationContext(), CenterActivity.class);
+            startActivity(intent);
+        }
+        if (view == project){
+            Intent intent = new Intent(getApplicationContext(), ProjectPlanActivity.class);
+            startActivity(intent);
+        }
+        if (view == trade){
+            Intent intent = new Intent(getApplicationContext(), TradeActivity.class);
+            startActivity(intent);
+        }
+        if (view == batch){
+            Intent intent = new Intent(getApplicationContext(), BatchActivity.class);
+            startActivity(intent);
+        }
+        if (view == tradeAndBatch){
+            Intent intent = new Intent(getApplicationContext(), TradeAndBatchActivity.class);
+            startActivity(intent);
+        }
+        if (view == time){
+            Intent intent = new Intent(getApplicationContext(), TimeActivity.class);
+            startActivity(intent);
+        }
+    }
 }
